b13a2f065fc13d5ad023460ea9977e0c567e454a
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-05-06 14:40:35 +0530

    reacr-router-and-firebase-installed

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRC15KVWQv6kO-M-xh00NglYOpHNJbar4",
+  authDomain: "warehouse-management-pro-7fb8c.firebaseapp.com",
+  projectId: "warehouse-management-pro-7fb8c",
+  storageBucket: "warehouse-management-pro-7fb8c.appspot.com",
+  messagingSenderId: "705562997391",
+  appId: "1:705562997391:web:eb75b1373eabca543836fa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
